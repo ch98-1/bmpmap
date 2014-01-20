8b29e12714fd32486aaa4984137029fdfa1f19ee
ch98-1 <akerunakayama+1@gmail.com>
2014-01-19 18:13:08 -0800

    update

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
-function bmpmap.getnode (ce, ci, cw, x, y, z)
+function bmpmap.getnode (ca, cb, cc, x, y, z)
  --configure here
  -- list of required nodes
  local dirt  = minetest.get_content_id("default:dirt")
  local air = minetest.get_content_id("air")
  --configure stuff at mapgen.lua too
-
+return air
 end
  
