bddc624284181943124eeba60a6897f18f64275a
ch98-1 <akerunakayama+1@gmail.com>
2014-01-23 17:23:48 -0800

    Delete conf.lua

--- conf.lua
+++
@@ -1,9 +0,0 @@
-function bmpmap.getnode (ca, cb, cc, x, y, z)
- --configure here
- -- list of required nodes
- local dirt  = minetest.get_content_id("default:dirt")
- local air = minetest.get_content_id("air")
- --configure stuff at mapgen.lua too
-return air
-end
- 
